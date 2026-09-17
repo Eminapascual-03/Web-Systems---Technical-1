b9a63c914185ed9f84004e53f50252983279eb73
Emina <ellieshaeminapaascual@gmail.com>
2026-09-18 04:02:35 +0800

    reformat: fixed the home route

--- tfa1/app/Config/Routes.php
+++ tfa1/app/Config/Routes.php
@@ -5,8 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /** @var RouteCollection $routes */
 // $routes->get('/', 'Home::index');
 
-$routes->get('/', 'Pages::home');
-$routes->get('/about', 'Pages::about');
-$routes->get('/customers', 'Customers::index');
-
-$routes->get('/users', 'Users::index');
+$routes->get('/', 'Home::index');
+$routes->get('home', 'Home::index');
+$routes->get('about', 'Pages::about');
+$routes->get('customers', 'Customers::index');
+$routes->get('users', 'Users::index');
